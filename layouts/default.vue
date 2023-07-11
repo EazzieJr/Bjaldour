54099c85855ac13145a6e3da95eb3849b2babee5
Ganiyu idris <golamide27@gmail.com>
2023-07-11 20:23:06 +0100

    Created a fucntion to check for screen width

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,32 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
-	
+	methods: {
+		...mapMutations(['setIsMobile']),
+
+		getCurrentScreen() {
+			this.currentScreen = window.innerWidth
+
+			window.addEventListener("resize", () => {
+				this.currentScreen = window.innerWidth
+
+				if (this.currentScreen < 767) {
+					this.setIsMobile(true)
+				} else {
+					this.setIsMobile(false)
+				}
+			})
+
+			if (this.currentScreen < 767) {
+				this.setIsMobile(true)
+			} else {
+				this.setIsMobile(false)
+			}
+		}, 
+	}
 }
 </script>
 
