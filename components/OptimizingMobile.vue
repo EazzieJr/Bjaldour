ff17f14578749ceb4e3cd92ed3d1a8bbdd9b0f11
Ganiyu idris <golamide27@gmail.com>
2023-07-23 22:37:05 +0100

    Finished challenges section of transtech on mobile

--- components/OptimizingMobile.vue
+++ components/OptimizingMobile.vue
@@ -35,6 +35,30 @@
 				</div>
 			</div>
 		</div>
+
+		<section class="Challenges">
+			<div class="Container">
+				<Tag text="Challenge" />
+
+				<div class="Content">
+					<div class="Texts">
+						<span>
+							TransTech, a leading transportation and logistics company, faced challenges in optimizing their operations, improving route efficiency, and reducing maintenance costs. They sought a solution that could leverage the power of machine learning to streamline their logistics processes and enhance overall operational efficiency.
+						</span>
+
+						<p>
+							Welcome to Bjaldour, where cutting-edge AI technology meets innovative SaaS products and tech hardware solutions. We're revolutionizing industries by harnessing the power of artificial intelligence, machine learning, and deep learning to create products that drive meaningful change. Explore our website and unlock a world of possibilities for your business.
+						</p>
+					</div>
+
+					<div class="Images">
+						<img src="/images/tt-challenge-img-1.png" alt="">
+						<img src="/images/tt-challenge-img-2.png" alt="">
+						<img src="/images/tt-challenge-img-3.png" alt="">
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
 
@@ -92,6 +116,48 @@ export default {
 				}
 			}
 		}
+	}
+
+	.Challenges {
+		@apply mt-28 px-5;
+
+		.Container {
+			@apply space-y-5 sm:space-y-0 sm:flex justify-between items-start;
+
+			.Content {
+				@apply pl-8 space-y-10 sm:w-[62.5vw];
+
+				.Texts {
+					@apply space-y-5;
+
+					span {
+						@apply text-2xl leading-[140%] tracking-[-0.025em]
+					}
+
+					p {
+						@apply leading-[140%]
+					}
+				}
+
+				.Images {
+					@apply flex items-end;
+					
+					img:nth-child(1) {
+						@apply w-28 shrink-0 h-auto
+					}
+					img:nth-child(2) {
+						@apply w-32 shrink-0 h-auto ml-3
+					}
+					img:nth-child(3) {
+						@apply w-14 shrink-0 h-auto -ml-5 -mb-10
+					}
+				}
+			}
+		}
+	}
+
+	.Obectives {
+		
 	}
 }
 </style>
